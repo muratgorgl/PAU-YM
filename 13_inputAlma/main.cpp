ee63ddeac111d4db9aa3834eef6cd0bde6951f63
Murat Görgülü <muratgorgl@gmail.com>
2025-05-08 11:56:10 +0300

    Add files via upload

---
+++ 13_inputAlma/main.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+int main()
+{
+/* 
+    string name;
+    int en, boy, alan;
+
+    cin >> name;
+    cout << "Sayin " << name << " Geometrik Alan Hesaplamaya Hosgeldiniz" << endl;
+    cout << "Lutfen Dikdortgenin Enini Giriniz: ";
+    cin >> en;
+    cout << "Lutfen Dikdortgenin Boyunu Giriniz: ";
+    cin >> boy;
+    alan = (en*boy);
+    cout << "Dikdortgenin Alani: " << alan << endl;
+*/
+
+/* 
+    int sonuc, x, y;
+    cout << "Lutfen Ussu Alinacak Sayiyi Giriniz: ";
+    cin >> x;
+
+    cout << "Lutfen Ussu Giriniz: ";
+    cin >> y;
+
+    sonuc = pow(x,y);
+    cout << sonuc << endl;
+*/
+
+    int sonuc;
+    sonuc = round(2.558);
+    cout << sonuc << endl;
+    return 0;
+}
